b0b1888815a663e4551ea8fdabdf82e030a4889f
Vitokdrako <vitokdrako@gmail.com>
2023-11-01 15:20:45 +0100

    обробка

--- main.py
+++ main.py
@@ -9,3 +9,30 @@ def input_error(handler):
         except IndexError as e:
             return "Error: not enough parameters."
     return wrapped
+
+@input_error
+def add_contact(command, *args):
+    name, phone = args
+    contacts[name] = phone
+    return f"Added contact {name}"
+
+@input_error
+def change_phone(command, *args):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return f"Changed phone for {name}"
+    else:
+        raise KeyError(name)
+
+@input_error
+def show_phone(command, *args):
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        raise KeyError(name)
+
+@input_error
+def show_all(command, *args):
+    return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
